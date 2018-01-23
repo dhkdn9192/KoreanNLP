8d9506ceac959f46d3b1ff1da1dcab2bdd010ce0
dhkdn9192 <dhkdn9192@naver.com>
2018-01-24 08:32:57 +0900

    w2v_with_kowiki 수정  - 대용량 파일 읽기용 코드 수정  - kowiki-bayes_classification.py 추가, 아직 코드는 작성 안 함

--- main/w2v_with_kowiki/kowiki_w2v_01-make_wakati.py
+++ main/w2v_with_kowiki/kowiki_w2v_01-make_wakati.py
@@ -3,37 +3,37 @@
 한국어 위키백과 텍스트 데이터를 이용한 word2vec 학습 - (1)
     - 위키 텍스트를 읽고 형태소 분석된 wakati 파일 만들기
 """
-import codecs
-# from bs4 import BeautifulSoup
 from konlpy.tag import Twitter
-# from gensim.models import word2vec
 
 
+# 형태소 분석된 라인을 wakati 파일에 append하는 함수
+def append_to_file(input, file):
+    with open(file, 'a', encoding='utf-8') as writeFp:
+        writeFp.write(input + " ")
+
+
+wakati_file = "wiki.wakati"     # wakati 파일 이름
+line_cnt = 0                    # 위키 텍스트 라인 인덱스 변수
+twitter = Twitter()             # 형태소 분석기 인스턴스
+
+# with open(wakati_file, 'a', encoding='utf-8') as writeFp:
 # 파일 열기
-readFp = codecs.open('../data/wiki.txt', 'r', encoding='utf-8')
-wakati_file = "wiki.wakati"
-writeFp = open(wakati_file, 'w', encoding='utf-8')
+with open('../data/wiki.txt', 'r', encoding='utf-8') as readFp:
+    for line in readFp:
+        if not line:
+            print("\n>> [INFO] not line, break : line_cnt=%.f" % line_cnt)
+            break
 
-# 형태소 분석
-twitter = Twitter()
+        if line_cnt % 1000 == 0:
+            print("current - " + str(line_cnt))
 
-i = 0
-while True:
-    line = readFp.readline()
-    if not line:
-        break
+        # 라인 단위 형태소 분석
+        malist = twitter.pos(line, norm=True, stem=True)
 
-    if i % 20000 == 0:
-        print("current - " + str(i))
-
-    # 라인 단위 형태소 분석
-    malist = twitter.pos(line, norm=True, stem=True)
-
-    # 필요한 어구만 대상으로 하기
-    for word in malist:
-        if not word[1] in ["Josa", "Eomi", "Punctuation"]:
-            writeFp.write(word[0] + " ")
-    i += 1
-
-writeFp.close()
+        # 필요한 어구만 대상으로 하기
+        for word in malist:
+            if not word[1] in ["Josa", "Eomi", "Punctuation"]:
+                append_to_file(word[0], wakati_file)
+                # writeFp.write(word[0] + " ")
+        line_cnt += 1
 
